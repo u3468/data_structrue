3e9b5e34e9ab5fff9d4282d6e18770c249539079
u3468 <u3468@163.com>
2015-11-07 21:38:21 +0800

    基本定义的修改

--- basic.c
+++ basic.c
@@ -8,8 +8,6 @@
 #define OVERFLOW -2
 #define LIST_INIT_SIZE 100
 #define LISTINCREMENT 10
-#define STACK_INIT_SIZE
-#define STACKINCREMENT 10
 typedef struct
 {
 	int *elem;
@@ -17,19 +15,8 @@ typedef struct
 	int listsize;
 }SqList;
 typedef int Status;
-typedef struct LNode
-{
-	int data;
-	struct LNode *next;
-}LNode, *LinkList;
-typedef struct
-{
-	int *base;
-	int *top;
-	int stacksize;
-}SqStack;
 typedef struct BiTNode
 {
-	int data;
+	char data;
 	struct BiTNode  *lchild, *rchild; 
 }BiTNode, *BiTree;
